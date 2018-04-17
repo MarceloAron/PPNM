74c7c00a35cde8c546406e011bfe6fa760435507
MarceloAron <marcelo.keni2@hotmail.com>
2018-04-17 10:57:10 +0200

    plot in .svg

--- l6/main-cmdline.c
+++ l6/main-cmdline.c
@@ -4,7 +4,7 @@
 int main(int argc, char** argv){
   for(int i=1; i<argc; i++) {
     double x = atof( argv[i] );
-    printf("%lg \t %lg\n",x,cos(x));
+    printf("%lg \t %lg\n",x,sin(x));
   }
   
   return 0;
--- l6/main-stdio.c
+++ l6/main-stdio.c
@@ -2,6 +2,6 @@
 #include<math.h>
 int main(){
   double x;
-  while(scanf("%lg",&x)!=EOF) printf("%lg \t %lg\n",x,sin(x));
+  while(scanf("%lg",&x)!=EOF) printf("%lg \t %lg\n",x,cos(x));
   return 0;
 }
